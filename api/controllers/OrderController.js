f7232727cabe937100a512c0ddffee73a5c23861
fuyaode <dan82625@gmail.com>
2015-11-03 17:02:50 +0800

    add deliveryTimeTypeStart query

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -58,6 +58,13 @@ OrderController = {
         query.username = ''
       }
 
+      if(query.deliveryTimeTypeStart && query.deliveryTimeTypeEnd){
+        queryShipmentObj.deliveryTimeType = { between : [new Date(query.deliveryTimeTypeStart), new Date(query.deliveryTimeTypeEnd)]};
+      }else if(query.deliveryTimeTypeStart || query.deliveryTimeTypeEnd) {
+        queryShipmentObj.deliveryTimeType = query.deliveryTimeTypeStart? { gte : new Date(query.deliveryTimeTypeStart)}: { lte : new Date(query.deliveryTimeTypeEnd)};
+      }
+      sails.log.info("!!!!!!!!!!!",queryShipmentObj);
+
       if(query.createdStart && query.createdEnd) {
          queryObj.createdAt = { between : [new Date(query.createdStart), new Date(query.createdEnd)]};
       }else if(query.createdStart || query.createdEnd) {
