393cf24aae8ea2dc58c7fe2f3ddb3c7b193079fe
fuyaode <dan82625@gmail.com>
2015-11-12 11:13:52 +0800

    remove log

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -110,7 +110,7 @@ AuthController = {
           } catch (e) {
             reference = { path : "" };
           }
-          console.log("!!!!!!!!!!!!!!!!",req.xhr);
+          
           if (req.xhr)
             return res.ok({
               status: "fail",
